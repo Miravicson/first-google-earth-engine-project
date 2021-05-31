2a24c40a9e342f1545f53aa2dc93bd472bc079b0
Victor Ughonu <victorughonu95@gmail.com>
2021-05-31 22:18:29 +0100

    Fixed error that occur with first wrongfully implemented auth.

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const EarthEngine = require('@google/earthengine');
-const {Router}  = require('express');
+const EarthEngine = require("@google/earthengine");
+const { Router } = require("express");
+const privateKey = require("./private-key.json");
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_EARTH_API_KEY;
 
-const TIMESERIES_URL 'https://code.googleearthengine.com/time-sries';
-const IMAGE_URL 'https://code.googleearthengine.com/images';
-
 
 function runAnalytics() {
- // Do your main stuff here
+  // Do your main stuff here
+
+  const image = new EarthEngine.Image("srtm90_v4");
+  image.getMap({ min: 0, max: 1000 }, function (map) {
+    console.log(map);
+  });
+
+  console.log("I am running analysis...");
 }
 
 function reportError(error) {
-  console.error('Initialization error: ' + e);
+  console.error("Initialization error: " + error);
 }
 
-
-
 function InitializeEGClient() {
-
-  EarthEngine.initialize(null, null, runAnalytics, reportError)
-
+  EarthEngine.initialize(null, null, runAnalytics, reportError);
 }
 
 function handleAuthenticationError(error) {
-  console.error('Authentication error: ' + e);
+  console.error("Authentication error: " + error);
 }
 
 function handleAuthenticateViaPopUp() {
-  EarthEngine.data.authenticateViaPopup(InitializeEGClient)
+  EarthEngine.data.authenticateViaPopup(InitializeEGClient);
 }
 
-
-EarthEngine.data.authenticateViaOauth(GOOGLE_CLIENT_ID, InitializeEGClient, handleAuthenticationError, null, handleAuthenticateViaPopUp);
-
-
-
-
+EarthEngine.data.authenticateViaPrivateKey(
+  privateKey,
+  InitializeEGClient,
+  handleAuthenticationError
+);
